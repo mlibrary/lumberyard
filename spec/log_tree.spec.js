aaaaf3a8ff997f334b19f1d29242479d9d479475
Matthew Alexander LaChance <mattlach@umich.edu>
2017-06-05 13:05:19 -0400

    :art: Declare tree var in the root description

--- spec/log_tree.spec.js
+++ spec/log_tree.spec.js
@@ -5,14 +5,16 @@
 const log_tree = require("../lib/log_tree");
 
 describe("a log_tree object", function() {
+  var tree;
+
   describe("when created from nothing", function() {
     it("has a length of 0", function() {
-      var tree = log_tree();
+      tree = log_tree();
       expect(tree.length()).toBe(0);
     });
   });
 
   it("can be created with a name", function() {
-    var tree = log_tree("name");
+    tree = log_tree("name");
   });
 });
