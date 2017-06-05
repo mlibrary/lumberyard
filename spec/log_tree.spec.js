aaaa1122f2e118ac7b8d7eb744043fd071865c2a
Matthew Alexander LaChance <mattlach@umich.edu>
2017-06-05 13:06:15 -0400

    :art: Merge two tests into a nested given

--- spec/log_tree.spec.js
+++ spec/log_tree.spec.js
@@ -5,16 +5,14 @@
 const log_tree = require("../lib/log_tree");
 
 describe("a log_tree object", function() {
-  it("can be created from nothing", function() {
-    var tree = log_tree();
+  describe("when created from nothing", function() {
+    it("has a length of 0", function() {
+      var tree = log_tree();
+      expect(tree.length()).toBe(0);
+    });
   });
 
   it("can be created with a name", function() {
     var tree = log_tree("name");
-  });
-
-  it("has a length of 0 when created from nothing", function() {
-    var tree = log_tree();
-    expect(tree.length()).toBe(0);
   });
 });
