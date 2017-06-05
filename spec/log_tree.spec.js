aaaa9352740cb7332020b46b0d4d2c201fe9057e
Matthew Alexander LaChance <mattlach@umich.edu>
2017-06-05 12:51:53 -0400

    :white_check_mark: log_tree() is a function

--- spec/log_tree.spec.js
+++ spec/log_tree.spec.js
@@ -2,4 +2,10 @@
 // All Rights Reserved. Licensed according to the terms of the Revised
 // BSD License. See LICENSE.txt for details.
 
-const LogTree = require("../lib/log_tree");
+const log_tree = require("../lib/log_tree");
+
+describe("a log_tree object", function() {
+  it("can be created from nothing", function() {
+    var tree = log_tree();
+  });
+});
