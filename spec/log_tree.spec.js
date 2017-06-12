aaaaedd330fa47dff4a40a9e5d88398360583b81
Matthew Alexander LaChance <mattlach@umich.edu>
2017-06-12 11:50:43 -0400

    :art: Use arrow functions

--- spec/log_tree.spec.js
+++ spec/log_tree.spec.js
@@ -5,97 +5,97 @@
 const log_tree = require("../lib/log_tree");
 var tree;
 
-describe("a log_tree with no description and no children", function() {
-  beforeEach(function() {
+describe("a log_tree with no description and no children", () => {
+  beforeEach(() => {
     tree = log_tree({});
   });
 
-  it("has a description of ''", function() {
+  it("has a description of ''", () => {
     expect(tree.description).toBe("");
   });
 
-  it("has a denominator of 1", function() {
+  it("has a denominator of 1", () => {
     expect(tree.den()).toBe(1);
   });
 
-  it("has a numerator of 0", function() {
+  it("has a numerator of 0", () => {
     expect(tree.num()).toBe(0);
   });
 
-  it("expands into an empty array", function() {
+  it("expands into an empty array", () => {
     expect([...tree]).toEqual([]);
   });
 
-  describe("when told that [] is complete", function() {
-    beforeEach(function() {
+  describe("when told that [] is complete", () => {
+    beforeEach(() => {
       tree.complete('[1496756029, "done", "it finished"]');
     });
 
-    it("has a numerator of 1", function() {
+    it("has a numerator of 1", () => {
       expect(tree.num()).toBe(1);
     });
   });
 });
 
-describe("a log_tree with two children", function() {
-  beforeEach(function() {
+describe("a log_tree with two children", () => {
+  beforeEach(() => {
     tree = log_tree({c:[{}, {}]});
   });
 
-  it("has a denominator of 3", function() {
+  it("has a denominator of 3", () => {
     expect(tree.den()).toBe(3);
   });
 
-  it("expands into a two-item array", function() {
+  it("expands into a two-item array", () => {
     expect([...tree].length).toBe(2);
   });
 });
 
-describe("a log_tree with two children and a grandchild", function() {
-  beforeEach(function() {
+describe("a log_tree with two children and a grandchild", () => {
+  beforeEach(() => {
     tree = log_tree({c:[{}, {c:[{}]}]});
   });
 
-  it("has a denominator of 4", function() {
+  it("has a denominator of 4", () => {
     expect(tree.den()).toBe(4);
   });
 
-  it("expands into a two-item array", function() {
+  it("expands into a two-item array", () => {
     expect([...tree].length).toBe(2);
   });
 
-  it("can reach its grandchild", function() {
+  it("can reach its grandchild", () => {
     expect([...[...tree][1]].length).toBe(1);
   });
 
-  it("has a numerator of 0", function() {
+  it("has a numerator of 0", () => {
     expect(tree.num()).toBe(0);
   });
 
-  describe("when told that [0] is complete", function() {
+  describe("when told that [0] is complete", () => {
     var children;
 
-    beforeEach(function() {
+    beforeEach(() => {
       tree.complete('[1496756029, "done", 0, "it finished"]');
       children = [...tree];
     });
 
-    it("has a numerator of 1", function() {
+    it("has a numerator of 1", () => {
       expect(tree.num()).toBe(1);
     });
 
-    it("has a first child with a numerator of 1", function() {
+    it("has a first child with a numerator of 1", () => {
       expect(children[0].num()).toBe(1);
     });
   });
 });
 
-describe("a log_tree with a description and no children", function() {
-  beforeEach(function() {
+describe("a log_tree with a description and no children", () => {
+  beforeEach(() => {
     tree = log_tree({d:"specification process"});
   });
 
-  it("stores its description", function() {
+  it("stores its description", () => {
     expect(tree.description).toBe("specification process");
   });
 });
