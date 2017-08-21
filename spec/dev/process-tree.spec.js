aaaa1ce7608293507c6cdc33afa3fcee966faae6
Matthew Alexander LaChance <mattlach@umich.edu>
2017-08-21 11:02:55 -0400

    :house: Move specs into spec/dev/

--- spec/dev/process-tree.spec.js
+++ spec/dev/process-tree.spec.js
@@ -2,7 +2,7 @@
 // All Rights Reserved. Licensed according to the terms of the Revised
 // BSD License. See LICENSE.txt for details.
 
-const processTree = require("../lib/process-tree");
+const processTree = require("../../lib/process-tree");
 
 let treeSpy = function(logTree) {
   let spy = { };
