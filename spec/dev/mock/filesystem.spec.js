aaaa238f89d5c951cac3cade4df3ce112927247c
Matthew Alexander LaChance <mattlach@umich.edu>
2017-09-15 13:48:52 -0400

    :white_check_mark: Still streams as expected for existing files

--- spec/dev/mock/filesystem.spec.js
+++ spec/dev/mock/filesystem.spec.js
@@ -81,6 +81,14 @@ describe("an empty filesystem mock", () => {
           done();
         });
       });
+
+      it("streams 'some contents' when fake_file.txt is read", done => {
+        let stream = fsMock.createReadStream("fake_file.txt");
+        stream.on("data", function(chunk) {
+          expect(chunk).toBe("some contents");
+          done();
+        });
+      });
     });
   });
 });
