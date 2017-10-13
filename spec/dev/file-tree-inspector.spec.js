aaaa6a4fb93c9de102b70356d53179233353b66d
Matthew Alexander LaChance <mattlach@umich.edu>
2017-10-13 11:43:30 -0400

    :art: Prep fs environment

--- spec/dev/file-tree-inspector.spec.js
+++ spec/dev/file-tree-inspector.spec.js
@@ -3,12 +3,80 @@
 // BSD License. See LICENSE.txt for details.
 
 const FileTreeInspector = require("../../lib/file-tree-inspector");
-inspector = null;
+const fs = require("fs");
+
+let inspector = null;
+
+let writeFile = (path, data) => new Promise(function(resolve, reject) {
+  fs.writeFile(path, data, function(err) {
+    if (err)
+      reject(err);
+    else
+      resolve(path);
+  });
+});
+
+let rm = path => new Promise(function(resolve, reject) {
+  fs.unlink(path, function(err) {
+    if (err)
+      reject(err);
+    else
+      resolve(path);
+  });
+});
+
+let mkdir = path => new Promise(function(resolve, reject) {
+  fs.mkdir(path, function(err) {
+    if (err)
+      reject(err);
+    else
+      resolve(path);
+  });
+});
+
+let rmdir = path => new Promise(function(resolve, reject) {
+  fs.rmdir(path, function(err) {
+    if (err)
+      reject(err);
+    else
+      resolve(path);
+  });
+});
 
 describe("an instance of FileTreeInspector()", () => {
   beforeEach(() => {
     inspector = FileTreeInspector();
   });
 
-  it("can be created", () => {});
+  describe("given a tree of files and directories", () => {
+    beforeEach(done => {
+      let reject = err => {
+        expect(err).toBe("not an error");
+        done();
+      };
+
+      mkdir("fstest").then(() => {
+        writeFile("fstest/a.txt", "Hey there").then(() => {
+          done();
+
+        }, reject);
+      }, reject);
+    });
+
+    it("can be created", () => {});
+
+    afterEach(done => {
+      let reject = err => {
+        expect(err).toBe("not an error");
+        done();
+      };
+
+      rm("fstest/a.txt").then(() => {
+        rmdir("fstest").then(() => {
+          done();
+
+        }, reject);
+      }, reject);
+    });
+  });
 });
