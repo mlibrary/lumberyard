aaaa2a83c20a374d7cba2c21250b01eb402561f5
Matthew Alexander LaChance <mattlach@umich.edu>
2017-10-13 11:25:49 -0400

    :white_check_mark: FileTreeInspector() exists

---
+++ spec/dev/file-tree-inspector.spec.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2017 The Regents of the University of Michigan.
+// All Rights Reserved. Licensed according to the terms of the Revised
+// BSD License. See LICENSE.txt for details.
+
+const FileTreeInspector = require("../../lib/file-tree-inspector");
+inspector = null;
+
+describe("an instance of FileTreeInspector()", () => {
+  beforeEach(() => {
+    inspector = FileTreeInspector();
+  });
+
+  it("can be created", () => {});
+});
