aaaa1b2a665ff6b130338931a789b48adf318e64
Matthew Alexander LaChance <mattlach@umich.edu>
2017-10-15 19:57:27 -0400

    :white_check_mark: Create fsWatcher

---
+++ spec/dev/fs-watcher.spec.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2017 The Regents of the University of Michigan.
+// All Rights Reserved. Licensed according to the terms of the Revised
+// BSD License. See LICENSE.txt for details.
+
+const fsWatcher = require("../../lib/fs-watcher");
