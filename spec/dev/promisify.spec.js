aaaa6e467ce0f5f99bfa665f5f4a1484133291fd
Matthew Alexander LaChance <mattlach@umich.edu>
2017-11-02 12:40:50 -0400

    :white_check_mark: promisify() is a function

---
+++ spec/dev/promisify.spec.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2017 The Regents of the University of Michigan.
+// All Rights Reserved. Licensed according to the terms of the Revised
+// BSD License. See LICENSE.txt for details.
+
+const expect = require("chai").expect;
+const promisify = require("../../lib/promisify.js");
+
+let promisedFunction;
+
+describe("my own homescripted promisify() function", () => {
+  describe("when given a function taking only a callback", () => {
+    beforeEach(() => {
+      promisedFunction = promisify(callback => {
+        callback();
+      });
+    });
+
+    it("returns a promise-based function", () => {
+    });
+  });
+});
