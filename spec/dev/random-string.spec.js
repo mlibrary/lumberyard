aaaaf6e14d1df3a49b1cb5f2d1d5e54fdd15f3f1
Matthew Alexander LaChance <mattlach@umich.edu>
2017-11-03 09:00:25 -0400

    :white_check_mark: Create randomString() function

---
+++ spec/dev/random-string.spec.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2017 The Regents of the University of Michigan.
+// All Rights Reserved. Licensed according to the terms of the Revised
+// BSD License. See LICENSE.txt for details.
+
+const expect = require("chai").expect;
+
+const randomString = require("../../lib/random-string");
