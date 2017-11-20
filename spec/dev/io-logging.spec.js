aaaab6d52284e1d6fa1ff1b573b818614bc2ee39
Matthew Alexander LaChance <mattlach@umich.edu>
2017-11-20 11:51:34 -0500

    :white_check_mark: IOLogging.ProcessTree() logs messages

--- spec/dev/io-logging.spec.js
+++ spec/dev/io-logging.spec.js
@@ -7,8 +7,30 @@
 const expect = require("chai").expect;
 const IOLogging = require("../../lib/io-logging");
 
-describe("IOLogging.ProcessTree()", () => {
+let logSet;
+let tree;
+
+describe("IOLogging.ProcessTree({})", () => {
   it("returns an object", () => {
-    expect(IOLogging.ProcessTree()).to.exist;
+    expect(IOLogging.ProcessTree({})).to.exist;
+  });
+});
+
+describe("IOLogging.ProcessTree({log: logFn})", () => {
+  beforeEach(() => {
+    logSet = new Set();
+    tree = IOLogging.ProcessTree({"log": x => { logSet.add(x); }});
+  });
+
+  describe("when run with a one-node tree", () => {
+    beforeEach(() => {
+      return tree(root => {
+        root.description = "just the one";
+      });
+    });
+
+    it("logs four messages", () => {
+      expect(logSet.size).to.equal(4);
+    });
   });
 });
