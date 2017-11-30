aaaa9c9a9e7bd0775bfc8cb8bc737fe9c7cc22bb
Matthew Alexander LaChance <mattlach@umich.edu>
2017-11-30 12:04:01 -0500

    :white_check_mark: errorDisplay.lines() is a function

--- spec/dev/error-display.spec.js
+++ spec/dev/error-display.spec.js
@@ -6,3 +6,23 @@
 /* eslint-disable no-unused-vars */
 const expect = require("chai").expect;
 const errorDisplay = require("../../lib/error-display");
+
+let error, lines;
+
+describe("a single error on a single node", () => {
+  beforeEach(() => {
+    error = Error();
+    error.description = "one node";
+    error.messages = [Error("uh oh")];
+    error.children = [];
+  });
+
+  describe("its array of lines", () => {
+    beforeEach(() => {
+      lines = errorDisplay.lines(error);
+    });
+
+    it("has 2 lines", () => {
+    });
+  });
+});
