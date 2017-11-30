aaaa16357db7b175414a152c3fe62e4b081ee6b5
Matthew Alexander LaChance <mattlach@umich.edu>
2017-11-30 11:44:47 -0500

    :white_check_mark: IOLogging.ProcessTree uses a real tree

--- spec/dev/io-logging.spec.js
+++ spec/dev/io-logging.spec.js
@@ -17,40 +17,146 @@ describe("IOLogging.ProcessTree()", () => {
 });
 
 describe("IOLogging.ProcessTree({log: logFn})", () => {
-  beforeEach(() => {
-    messages = new Set();
-    tree = IOLogging.ProcessTree({"log": x => { messages.add(x); }});
-  });
-
-  describe("when run with a one-node tree", () => {
+  describe("with a set of messages", () => {
     beforeEach(() => {
-      return tree(root => {
-        root.description = "just the one";
+      messages = new Set();
+      tree = IOLogging.ProcessTree({"log": x => { messages.add(x); }});
+    });
+
+    describe("when run with a one-node tree", () => {
+      beforeEach(() => {
+        return tree(root => {
+          root.description = "just the one";
+        });
+      });
+
+      it("logs four messages", () => {
+        expect(messages.size).to.equal(4);
+      });
+
+      it("says 'beginning setup and validation'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;32m *\x1b[0m Beginning setup and validation ...");
+      });
+
+      it("says 'finished setup and validation'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;32m *\x1b[0m Finished setup and validation (0/1) ...");
+      });
+
+      it("says 'finished just the one'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;32m *\x1b[0m Finished just the one (1/1) ...");
+      });
+
+      it("says 'done'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;32m *\x1b[0m Done.");
       });
     });
 
-    it("logs four messages", () => {
-      expect(messages.size).to.equal(4);
+    describe("when run with a tree with a child", () => {
+      beforeEach(() => {
+        return tree(root => {
+          root.description = "the parent";
+
+          root.add(child => {
+            child.description = "the child";
+          });
+        });
+      });
+
+      it("logs five messages", () => {
+        expect(messages.size).to.equal(5);
+      });
+
+      it("says 'beginning setup and validation'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;32m *\x1b[0m Beginning setup and validation ...");
+      });
+
+      it("says 'finished setup and validation'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;32m *\x1b[0m Finished setup and validation (0/2) ...");
+      });
+
+      it("says 'finished the child'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;32m *\x1b[0m Finished the child (1/2) ...");
+      });
+
+      it("says 'finished the parent'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;32m *\x1b[0m Finished the parent (2/2) ...");
+      });
+
+      it("says 'done'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;32m *\x1b[0m Done.");
+      });
     });
 
-    it("says 'beginning setup and validation'", () => {
-      expect(messages).to.contain(
-        "\x1b[1;32m *\x1b[0m Beginning setup and validation ...");
+    describe("when run with a lot of extra log messages", () => {
+      beforeEach(() => {
+        return tree(root => {
+          root.description = "rooot";
+
+          root.run = () => {
+            root.log("info", ":)");
+            root.log("warn", ":|");
+            root.log("error", ":(");
+            root.log("weird", ":o");
+          };
+        });
+      });
+
+      it("logs seven messages", () => {
+        expect(messages.size).to.equal(7);
+      });
+
+      it("says ':)'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;32m *\x1b[0m :)");
+      });
+
+      it("says ':|'", () => {
+        expect(messages).to.contain(
+          "\x1b[1;33m *\x1b[0m :|");
+      });
+
+      it("says ':('", () => {
+        expect(messages).to.contain(
+          "\x1b[1;31m *\x1b[0m :(");
+      });
+
+      it("doesn't say ':o'", () => {
+        for (const m of messages)
+          expect(m).not.to.match(/:o$/);
+      });
+    });
+  });
+
+  describe("with a string of messages", () => {
+    beforeEach(() => {
+      messages = "";
+      tree = IOLogging.ProcessTree({"log": x => {
+        messages += x + "\n";
+      }});
     });
 
-    it("says 'finished setup and validation'", () => {
-      expect(messages).to.contain(
-        "\x1b[1;32m *\x1b[0m Finished setup and validation (0/1) ...");
-    });
+    describe("when run with a single invalid node", () => {
+      beforeEach(() => {
+        return tree(root => {
+          root.description = "hey now";
+          throw Error("uh oh");
+        });
+      });
 
-    it("says 'finished just the one'", () => {
-      expect(messages).to.contain(
-        "\x1b[1;32m *\x1b[0m Finished just the one (1/1) ...");
-    });
-
-    it("says 'done'", () => {
-      expect(messages).to.contain(
-        "\x1b[1;32m *\x1b[0m Done.");
+      it("errors after validation", () => {
+        expect(messages.slice(14, 75)).to.equal(
+          "Beginning setup and validation ...\n\x1b[1;31m *"
+          + "\x1b[0m Full JSON: {");
+      });
     });
   });
 });
