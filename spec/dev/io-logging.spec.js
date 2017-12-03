aaaa37add576b022ff1ef36cbc0df5d6afab3381
Matthew Alexander LaChance <mattlach@umich.edu>
2017-12-03 14:56:30 -0500

    :construction: Need better "tail -f" functionality

--- spec/dev/io-logging.spec.js
+++ spec/dev/io-logging.spec.js
@@ -4,7 +4,10 @@
 
 /* eslint-env mocha */
 /* eslint-disable no-unused-expressions */
+/* eslint-disable no-unused-vars */
 const expect = require("chai").expect;
+const fs = require("fs");
+const makePromise = require("../../lib/make-promise");
 const IOLogging = require("../../lib/io-logging");
 
 let messages;
@@ -152,10 +155,255 @@ describe("IOLogging.ProcessTree({log: logFn})", () => {
         });
       });
 
-      it("errors after validation", () => {
-        expect(messages.slice(14, 75)).to.equal(
-          "Beginning setup and validation ...\n\x1b[1;31m *"
-          + "\x1b[0m Full JSON: {");
+      it("errors after beginning validation", () => {
+        const match = messages.match(/^[^{]+\{/);
+        expect(match).to.exist;
+
+        expect(match[0]).to.equal(
+          "\x1b[1;32m *\x1b[0m Beginning setup and validation ...\n"
+          + "\x1b[1;31m *\x1b[0m Full JSON: {");
+      });
+
+      it("has the expected full json string", () => {
+        const match = messages.match(/\{.*\}/);
+        expect(match).to.exist;
+
+        expect(JSON.parse(match[0])).to.deep.equal({
+          "description": "hey now",
+          "messages": ["Error: uh oh"],
+          "children": []
+        });
+      });
+
+      it("ends with a readable printout", () => {
+        const lines = messages.split("\n");
+        expect(lines).to.have.lengthOf(5);
+        expect(lines[2]).to.equal("\x1b[1;31m *\x1b[0m hey now:");
+        expect(lines[3]).to.equal("\x1b[1;31m *\x1b[0m   Error: uh oh");
+        expect(lines[4]).to.equal("");
+      });
+    });
+
+    describe("when run with a node that fails at runtime", () => {
+      beforeEach(() => {
+        return tree(root => {
+          root.description = "you're an all-star";
+          root.run = () => {
+            throw Error("get your game on");
+          };
+        });
+      });
+
+      it("errors after finishing validation", () => {
+        const match = messages.match(/^[^{]+\{/);
+        expect(match).to.exist;
+
+        expect(match[0]).to.equal(
+          "\x1b[1;32m *\x1b[0m Beginning setup and validation ...\n"
+          + "\x1b[1;32m *\x1b[0m Finished setup and validation (0/1) ...\n"
+          + "\x1b[1;31m *\x1b[0m Full JSON: {");
+      });
+
+      it("has the expected full json string", () => {
+        const match = messages.match(/\{.*\}/);
+        expect(match).to.exist;
+
+        expect(JSON.parse(match[0])).to.deep.equal({
+          "description": "you're an all-star",
+          "messages": ["Error: get your game on"],
+          "children": []
+        });
+      });
+
+      it("ends with a readable printout", () => {
+        const lines = messages.split("\n");
+        expect(lines).to.have.lengthOf(6);
+        expect(lines[3]).to.equal("\x1b[1;31m *\x1b[0m you're an all-star:");
+        expect(lines[4]).to.equal("\x1b[1;31m *\x1b[0m   Error: get your game on");
+        expect(lines[5]).to.equal("");
+      });
+    });
+
+    xdescribe("when given a logfile", function() {
+      this.timeout(6000);
+      let lines, jsons, itSucceeded;
+
+      const logfile = "io_logtest_out.jsonl";
+      const rm = makePromise(fs.unlink);
+      const read = makePromise(fs.readFile);
+
+      const runAndRead = function(callback) {
+        lines = null;
+        jsons = null;
+        itSucceeded = null;
+
+        return tree(logfile, callback).then(() => {
+          return read(logfile);
+        }).then(data => {
+          lines = data.toString().split("\n");
+          expect(lines.pop()).to.equal("");
+
+          jsons = [];
+          for (const line of lines)
+            jsons.push(JSON.parse(line));
+
+          expect(messages).to.equal("");
+
+          return tree(logfile).then(() => {
+            itSucceeded = true;
+          }, () => {
+            itSucceeded = false;
+          });
+        });
+      };
+
+      afterEach(() => {
+        return rm(logfile);
+      });
+
+      describe("and run with a single node", () => {
+        beforeEach(() => {
+          return runAndRead(root => {
+            root.description = "root";
+          });
+        });
+
+        it("outputs three json lines", () => {
+          expect(jsons).to.have.lengthOf(3);
+        });
+
+        it("posts the logtree first", () => {
+          expect(jsons[0][1]).to.equal("log-tree");
+          expect(jsons[0][2]).to.deep.equal({
+            "c": [],
+            "d": "root"
+          });
+        });
+
+        it("posts a 'begin' message second", () => {
+          expect(jsons[1]).to.have.lengthOf(3);
+          expect(jsons[1][1]).to.equal("begin");
+        });
+
+        it("posts a 'done' message last", () => {
+          expect(jsons[2]).to.have.lengthOf(3);
+          expect(jsons[2][1]).to.equal("done");
+        });
+
+        it("can be converted into bulleted text", () => {
+          expect(messages).to.equal(
+            "\x1b[1;32m *\x1b[0m Beginning setup and validation ...\n"
+            + "\x1b[1;32m *\x1b[0m Finished setup and validation (0/1) ...\n"
+            + "\x1b[1;32m *\x1b[0m Finished root (1/1) ...\n"
+            + "\x1b[1;32m *\x1b[0m Done.\n");
+        });
+      });
+
+      describe("and run with four nodes", () => {
+        beforeEach(() => {
+          return runAndRead(root => {
+            root.description = "parent";
+
+            root.add(child => {
+              child.description = "first child";
+
+              child.add(grandchild => {
+                grandchild.description = "grandchild";
+              });
+            });
+
+            root.add(child => {
+              child.description = "second child";
+            });
+          });
+        });
+
+        it("outputs nine json lines", () => {
+          expect(jsons).to.have.lengthOf(9);
+        });
+
+        it("posts the logtree first", () => {
+          expect(jsons[0][1]).to.equal("log-tree");
+          expect(jsons[0][2]).to.deep.equal({
+            "d": "parent",
+            "c": [{
+              "d": "first child",
+              "c": [{
+                "d": "grandchild",
+                "c": []
+              }]
+            }, {
+              "d": "second child",
+              "c": []
+            }]
+          });
+        });
+      });
+
+      describe("and run with an invalid node", () => {
+        beforeEach(() => {
+          return runAndRead(root => {
+            root.description = "come in here dear boy";
+            throw Error("have a cigar");
+          });
+        });
+
+        it("outputs one json line", () => {
+          expect(jsons).to.have.lengthOf(1);
+        });
+
+        it("only posts an error", () => {
+          expect(jsons[0][1]).to.equal("fatal");
+          expect(jsons[0][2]).to.deep.equal({
+            "description": "come in here dear boy",
+            "messages": ["Error: have a cigar"],
+            "children": []
+          });
+        });
+
+        it("can be converted into bulleted text", () => {
+          expect(messages).to.equal(
+            "\x1b[1;32m *\x1b[0m Beginning setup and validation ...\n"
+            + "\x1b[1;31m *\x1b[0m come in here dear boy:\n"
+            + "\x1b[1;31m *\x1b[0m   Error: have a cigar\n");
+        });
+      });
+
+      describe("and run with a failing node", () => {
+        beforeEach(() => {
+          return runAndRead(root => {
+            root.description = "you're gonna go far";
+
+            root.run = () => {
+              throw Error("you're gonna fly high");
+            };
+          });
+        });
+
+        it("outputs three json lines", () => {
+          expect(jsons).to.have.lengthOf(3);
+        });
+
+        it("posts the logtree first", () => {
+          expect(jsons[0][1]).to.equal("log-tree");
+          expect(jsons[0][2]).to.deep.equal({
+            "d": "you're gonna go far",
+            "c": []
+          });
+        });
+
+        it("logs its beginning", () => {
+          expect(jsons[1][1]).to.equal("begin");
+        });
+
+        it("finishes with an error", () => {
+          expect(jsons[2][1]).to.equal("fatal");
+          expect(jsons[2][2]).to.deep.equal({
+            "description": "you're gonna go far",
+            "messages": ["Error: you're gonna fly high"],
+            "children": []
+          });
+        });
       });
     });
   });
